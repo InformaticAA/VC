6c6275b4716b3ac479101daaf01a58e466f7fbb0
schreilem <566400@unizar.es>
2015-04-15 12:50:17 +0200

    Initial commit

---
+++ T4/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * main.cpp
+ *
+ * Autores: Alejandro Marquez Ferrer (566400)
+ * 			Alejandro Royo Amondarain (560285)
+ *
+ * Descripcion: Este fichero contiene el codigo correspondiente
+ * 				al programa principal. Se encarga de realizar la
+ * 				operacion solicitada por el usuario, ya sea
+ * 				ejecutar el apartado 1, 2 o el opcional (solo
+ * 				para una imagen).
+ *
+ */
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+
+using namespace std;
+using namespace cv;
+
+int main(int, char**){
+
+
+}
+
