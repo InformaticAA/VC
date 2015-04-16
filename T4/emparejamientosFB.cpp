192748cbf85eee24172f746349b5ed01b697a2f1
Alejandro <donrondon40@gmail.com>
2015-04-16 16:22:03 +0200

    BFMatcher arreglado

--- T4/emparejamientosFB.cpp
+++ T4/emparejamientosFB.cpp
@@ -11,10 +11,9 @@ using namespace cv;
 void emparejamientos(Mat &i1, Mat &i2);
 
 void emparejamientos(Mat &i1, Mat &i2){
-	Mat d1, d2;
+	Mat d1, d2, i_matches;
 	vector< KeyPoint > kp1, kp2;
-	vector< int > matches;
-	Mat i_matches;
+	vector< DMatch > matches;
 
 	SurfFeatureDetector detector(400);
 	detector.detect(i1,kp1);
@@ -24,12 +23,19 @@ void emparejamientos(Mat &i1, Mat &i2){
 	extractor.compute(i1,kp1,d1);
 	extractor.compute(i2,kp2,d2);
 
-	BruteForceMatcher< L2 < float > > matcher;
-	matcher.add(d2);
-	matcher.match(d1,matches);
+	cout << "Pre BF" << endl;
+
+	BFMatcher matcher(NORM_L2);
+	matcher.match(d1,d2,matches);
+
+	cout << "Post BF" << endl;
+
+//	BruteForceMatcher< L2 < float > > matcher;
+//	matcher.add(d2);
+//	matcher.match(d1,d2,matches);
 
 	namedWindow("matches",1);
-	drawMatches(i1,i2,kp1,kp2,matches,vector< char >(),i_matches);
+	drawMatches(i1,kp1,i2,kp2,matches,i_matches);
 	imshow("matches",i_matches);
 	waitKey(0);
 }
--- T4/main.cpp
+++ T4/main.cpp
@@ -9,10 +9,17 @@
 using namespace std;
 using namespace cv;
 
+extern void emparejamientos(Mat &i1, Mat &i2);
+
 int main(int, char**){
+
+	cout << "Pre imagenes" << endl;
 
 	Mat i1 = imread("Imagenes/castle1.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 	Mat i2 = imread("Imagenes/castle2.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 
+	cout << " imagenes bien " << endl;
+
+	emparejamientos(i1,i2);
 }
 
